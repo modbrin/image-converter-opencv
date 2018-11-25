ed6286ecab740c3032acd203e2c46e3546389dd1
Maksim Surkov <modbrin@live.com>
2018-11-25 16:45:17 +0300

    Added support for command line passing of path to folder of source images

--- image_converter/converter.cpp
+++ image_converter/converter.cpp
@@ -7,19 +7,20 @@ void Converter::png_to_bmp(const fs::path& filepath) const
 {
 	cv::Mat image = cv::imread(filepath.string(), -1);
 	cv::Mat image_bmp;
-	image.convertTo(image_bmp, CV_8UC3);
+	image.convertTo(image_bmp, CV_32FC4);
 	fs::path write_path{ filepath };
 	write_path.replace_extension(".bmp");
 	imwrite(write_path.string(), image_bmp);
 }
 
-void Converter::batch_process_png(const bool& keep_original) const
+void Converter::batch_process_png(const cv::String folder_path, const bool& keep_original) const
 {
+	int counter = 0;
 	try {
-		fs::path path = fs::current_path().append("imgs_test\\");
+		const fs::path path{folder_path};
 		for (auto & p : fs::recursive_directory_iterator(path))
 		{
-			if (p.path().extension().compare(".jpg") == 0)
+			if (p.path().extension().compare(".png") == 0)
 			{
 				this->png_to_bmp(p.path());
 				std::cout << "Converted " << p.path() << std::endl;
@@ -27,13 +28,16 @@ void Converter::batch_process_png(const bool& keep_original) const
 				{
 					fs::remove(p.path());
 				}
+				++counter;
 			}
 		}
 	}
 	catch (fs::filesystem_error e)
 	{
 		std::cout << e.what() << std::endl;
+		exit(1);
 	}
+	std::cout << "Total converted: " << counter << std::endl;
 	system("pause");
 }
 
--- image_converter/converter.h
+++ image_converter/converter.h
@@ -7,13 +7,14 @@ public:
 	
 	/**
 	 * \brief Read png image by given path and create bmp copy next to it
-	 * \param filepath Path to original image
+	 * \param filepath Path to original image.
 	 */
 	void png_to_bmp(const std::filesystem::path& filepath) const;
 	
 	/**
 	 * \brief Find all png files in given folder to be deleted.
+	 * \param folder_path Path that will be scanned for images.
 	 * \param keep_original Set true to disable deletion of original files.
 	 */
-	void batch_process_png(const bool& keep_original) const;
+	void batch_process_png(const cv::String folder_path, const bool& keep_original) const;
 };
